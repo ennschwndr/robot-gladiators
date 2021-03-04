620b98ce0e2650de5befa4bf81a6a1fe615e2ef8
ennschwndr <77927857+ennschwndr@users.noreply.github.com>
2021-03-04 11:40:10 -0500

    shop function

---
+++ assets/js/game.js
@@ -0,0 +1,147 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(enemyName) {
+
+    while(enemyHealth > 0 && playerHealth > 0){
+
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose.");
+
+        if (promptFight === "skip" || promptFight === "SKIP"){
+            var confirmSkip = window.confirm("Are you sure that you want to skip?");
+
+            if (confirmSkip) {
+                window.alert(playerName + " has chosen to skip this fight.");
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
+            }
+        }  
+
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " remaining health."
+        );
+
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+            break;
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+
+        playerHealth = playerHealth - enemyAttack;
+
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " remaining health"
+        );
+
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+            break;
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+        
+        if (promptFight === "fight" || promptFight === "FIGHT"){}
+    }
+};    
+
+var startGame = function(){    
+   
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if(playerHealth > 0){
+            window.alert("Welcome to Robot-Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+            if (playerHealth > 0 && i < enemyNames.length -1){
+                var storeConfirm = window.confirm("The fight is over, so visit the store before the next round.")
+                if (storeConfirm){
+                    shop();
+                }
+            }
+        }else {
+            window.alert("You have lost ypur robot in battle! GAME OVER.");
+        }
+    }
+
+    endGame();
+};
+
+var endGame = function(){
+
+    if(playerHealth > 0){
+        window.alert("Great job! You survived the game. You now have a score of " + playerMoney + " .");
+    }
+    else {
+        window.alert("Your robot was lost in battle! You lost the game 😢")
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?"); 
+
+    if(playAgainConfirm){
+        startGame();
+    }
+    else{
+        window.alert("Thank you for playing Robot Gladiators! Come back soon.")
+    }   
+}
+
+var shop = function() {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your heatlh, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    )
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "refill":
+            if (playerMoney >= 7){
+                window.alert("Refilling player's health by 20 for 7 credits.")
+
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            }
+            else{
+                window.alert("You don't have eneough money!");
+            }
+        break;
+
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7){
+                window.alert("Upgrading player's attack by 6 for 7 credits.")
+
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            }
+            else{
+                window.alert("You don't have enough money!");
+            }
+        break;
+        
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.");
+            break;
+
+        default:
+            window.alert("You did not pick a valid option. Please try again.")
+
+        shop();
+        break;
+    }
+};
+
+startGame();
